916d9440bd3aff714123281777fb7e7cc64ee782
DavidG <Davidbhyss@gmail.com>
2023-02-09 17:21:07 -0700

    Corrected code for Lab Demonstration

--- Week04/Lab02/ConvertToHours.java
+++ Week04/Lab02/ConvertToHours.java
@@ -7,24 +7,23 @@ public class ConvertToHours {
         //variable declaration
         int hours, minutes, seconds, secondsremnant;
         
-
         SECONDS_PER_MINUTE = MINUTES_PER_HOUR = 60;
         SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR;
 
         //Scanner is used in order to get necessary input.
         Scanner scan = new Scanner(System.in);
-
+        //Message for user to enter an integer.
         System.out.println("Enter the total number of seconds: ");
-
+        //takes the integer entered by user.
         seconds = scan.nextInt();
-
+        //calculations needed to determine proper outputs.
         hours = seconds / MINUTES_PER_HOUR / SECONDS_PER_MINUTE;    
 
         minutes = seconds / MINUTES_PER_HOUR % SECONDS_PER_MINUTE;
 
         secondsremnant =  seconds % SECONDS_PER_MINUTE;
 
-        //Print statements needed in order to aquire my calculation outputs
+        //Print statements needed in order to acquire my calculation outputs.
 
         System.out.println("Total Seconds: " + seconds);
         System.out.println("Hours: " + hours);
