1bfb664806d7981b7db6fb4c8ce2bd497834e22c
DavidG <Davidbhyss@gmail.com>
2023-02-13 10:16:48 -0700

    Adding stuff

---
+++ Week05/README.txt
@@ -0,0 +1,6 @@
+Week 05 lecture
+
+Consists of how to use classes and objects
+
+We experiment with the random class to see its function
+and how it truly isn't a random operation.
--- Week05/Whiteboard.java
+++ Week05/Whiteboard.java
@@ -1,3 +1,11 @@
+import java.util.*;
+
+
 public class Whiteboard {
-    
+    public static void main(String[] args) {
+        Random gen = new Random();
+        Scanner scan = new Scanner(System.in);
+        String testing = "Here we are!"; 
+
+    }
 }
