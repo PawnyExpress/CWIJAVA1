f8f9e7f3d4f7e8a3e473cc8662882f84e29e5e83
DavidG <Davidbhyss@gmail.com>
2023-02-13 11:35:37 -0700

    Random Java Instructure

--- Week05/Whiteboard.java
+++ Week05/Whiteboard.java
@@ -3,9 +3,29 @@ import java.util.*;
 
 public class Whiteboard {
     public static void main(String[] args) {
-        Random gen = new Random();
+        Random generator = new Random(1234);
         Scanner scan = new Scanner(System.in);
         String testing = "Here we are!"; 
+        
+        int randomIntegers;
+        double randomDouble, randomPercent;
+        boolean isHeads;
 
+        /*  Enemy Alien
+         *  0.0-.3999   40% Move
+         *  .40-.6999   30% Fire Lasers
+         *  .70-.8999   20% Evade
+         *  .90-.9999   10% Collide 
+         */
+
+        randomIntegers = generator.nextInt(20) + 1;
+        randomDouble = 19 * generator.nextDouble() + 1;
+        randomPercent = generator.nextDouble();
+        isHeads =generator.nextBoolean();
+
+        System.out.println(randomIntegers);
+        System.out.println(randomDouble);
+        System.out.println(randomPercent);
+        System.out.println("It was Heads: " + isHeads);
     }
 }
