858dbfbb136491ca1d97517568deb40ae4d956d8
David Garcia <davidbhyss@gmail.com>
2023-02-26 21:15:38 -0700

    All of ch4 examples

---
+++ Week07/Ch4_Examples/Stars/src/New.java
@@ -0,0 +1,11 @@
+public class New {
+    public static void main(String[] args) {
+        int firstNum = 25;
+
+      int seconNum = 3;
+
+      double result = 25/3;
+
+      System.out.println(result);
+    }
+}
