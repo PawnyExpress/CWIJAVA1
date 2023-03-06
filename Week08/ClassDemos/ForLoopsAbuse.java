ae136c8146fd3dd733f2c11349a441acf15b70c2
DavidG <Davidbhyss@gmail.com>
2023-03-06 12:01:29 -0700

    Week o8 demos

---
+++ Week08/ClassDemos/ForLoopsAbuse.java
@@ -0,0 +1,8 @@
+public class ForLoopsAbuse {
+    public static void main(String[] args) {
+        
+        for (int count = 1; count <= 5 ; count++) {
+            System.out.println("*");
+        }
+    }
+}
