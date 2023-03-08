92a716685dc4d471958cfe33e7eb729f2ac725c1
David G <Davidbhyss@gmail.com>
2023-03-08 09:07:43 -0700

    Update

--- Project_01/ParkingFinder.java
+++ Project_01/ParkingFinder.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-
+import java.lang.Math;
 import edu.cwi.parking.ParkingSpot;
 
 public class ParkingFinder {
@@ -8,7 +8,7 @@ public class ParkingFinder {
         ParkingSpot tempspot, firstSpot, secondSpot, thirdSpot, fourthSpot;
         Scanner scan;
         long seed;
-        int parkingTime, carX, carY, randomX, randomY;
+        int parkingTime, carX, carY, randomX, randomY, distanceOne, distanceTwo,distanceThree,distanceFour;
         Random generator;
         
 
@@ -30,16 +30,24 @@ public class ParkingFinder {
         System.out.println("The position of your vehicle is: X:" + carX + " Y:" + carY);
 
         firstSpot = new ParkingSpot("Lenox Ave", randomX = generator.nextInt(100), randomY = generator.nextInt(100));
+        distanceOne = Math.abs(carX - firstSpot.getLocationX() ) + Math.abs(carY - firstSpot.getLocationY());
         secondSpot = new ParkingSpot("Russel Way", randomX = generator.nextInt(100), randomY = generator.nextInt(100));
+        distanceTwo = Math.abs(carX - secondSpot.getLocationX() ) + Math.abs(carY - secondSpot.getLocationY());
         thirdSpot = new ParkingSpot("Cherry Lane", randomX = generator.nextInt(100), randomY = generator.nextInt(100));
         thirdSpot.setCostPerInterval(0.30);
+        distanceThree = Math.abs(carX - thirdSpot.getLocationX() ) + Math.abs(carY - thirdSpot.getLocationY());
         fourthSpot = new ParkingSpot("Wisteria Lane", randomX = generator.nextInt(100), randomY = generator.nextInt(100));
         fourthSpot.setCostPerInterval(0.30);
+        distanceFour = Math.abs(carX - fourthSpot.getLocationX() ) + Math.abs(carY - fourthSpot.getLocationY());
 
         System.out.println(firstSpot);
+        System.out.println(distanceOne);
         System.out.println(secondSpot.toString());
+        System.out.println(distanceTwo);
         System.out.println(thirdSpot.toString());
+        System.out.println(distanceThree);
         System.out.println(fourthSpot.toString());
+        System.out.println(distanceFour);
         
 
         scan.close();
