af42b320f03de80c359f33a33df885bbfb2d7cbb
DavidG <Davidbhyss@gmail.com>
2023-04-05 09:53:33 -0600

    Week11 classwork

---
+++ Week11/Lab07/Box.java
@@ -0,0 +1,57 @@
+package Lab07;
+
+public class Box {
+    private double width;
+    private double height;
+    private double depth;
+    private boolean full;
+
+    public Box(Double width, Double height, Double depth) {
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public boolean getFull() {
+        return full;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth; 
+    }
+
+    public void setFull(boolean full) {
+        this.full = full;
+    }
+
+    public double volume() {
+
+    }
+
+    public double surfaceArea() {
+
+    }
+
+    public String toString () {
+
+    }
+}
+
---
+++ Week11/Lab07/BoxTest.java
@@ -0,0 +1,7 @@
+package Lab07;
+
+public class BoxTest {
+    public static void main(String[] args) {
+        
+    }
+}
